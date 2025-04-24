529f2e3c639df10b138811a7e4c24dbd9e86515a
CaioLira18 <caiozao1212@gmail.com>
2025-04-24 14:57:32 -0300

    Order, Instant, ISO 8602 - Part 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -23,6 +26,13 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy="client")
+    private List<Order> orders = new ArrayList<>();
+
+    
+
+
+    
 
     public User(){
 
@@ -74,6 +84,10 @@ public class User implements Serializable {
 
     public void setPassword(String passowrd) {
         this.password = passowrd;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     // Equals e HashCode
