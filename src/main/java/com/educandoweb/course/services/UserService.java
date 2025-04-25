ed20e02cebc33d699eac16251eb09157019a0718
CaioLira18 <caiozao1212@gmail.com>
2025-04-25 14:03:25 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
